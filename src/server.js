a98c0e496d7671e5bbb5b586840c38eff58a73f5
Amr-7246 <aehab7246@gmail.com>
2025-04-30 19:15:30 +0300

    Done

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 "use strict";
+import 'module-alias/register'
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
